da40469e7cc4517052db085a3f134d603e6deb2b
Dhanushraj Murugan <119844990+dhanushrajm@users.noreply.github.com>
2026-05-01 23:32:28 +0300

    Add files via upload

---
+++ Day 1 of R programming.R
@@ -0,0 +1,26 @@
+R.version.string
+
+library(tidyverse)
+
+head(mtcars)
+
+add <- 10+5
+multiply <- 2*10
+total <- sum(add, multiply)
+print(total)
+?sum
+
+ls()
+
+power <- 2^10
+
+rm(power)
+
+getwd(
+)
+
+a <- 75.7345
+round(a, digits=2)
+
+sqrt(100)
+abs(-100)
